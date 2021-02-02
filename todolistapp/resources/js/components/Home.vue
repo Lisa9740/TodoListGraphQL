1577e67b402a7bb3b2a7f3c982d403cd14513ea5
alison <alison@seyes.re>
2021-02-02 14:04:12 +0400

    Init todolistapp with laravel/vue/vuetify

---
+++ todolistapp/resources/js/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-app id="inspire">
+        <v-app-bar
+            app
+            clipped-left
+        >
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>Application</v-toolbar-title>
+        </v-app-bar>
+
+        <v-main>
+            <v-container>
+
+            </v-container>
+        </v-main>
+
+        <v-footer app>
+            <span>© {{ new Date().getFullYear() }}</span>
+        </v-footer>
+    </v-app>
+</template>
+
+<script>
+export default {
+    props: {
+        source: String,
+    },
+    data: () => ({
+        drawer: null,
+    }),
+    created () {
+        this.$vuetify.theme.dark = true
+    },
+}
+</script>
